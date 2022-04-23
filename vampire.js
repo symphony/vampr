8ad9c296e019a64228f12cc6a8ce20409a108c6a
symphony <jciurc@gmail.com>
2022-04-23 16:36:42 -0600

    updated find vampire to return early if vampire is found

--- vampire.js
+++ vampire.js
@@ -72,7 +72,12 @@ class Vampire {
   // = main traversal methods =
   // Returns the vampire object with that name, or null if no vampire exists with that name
   vampireWithName(name) {
-    return this.flattenOffspring(this).find(child => child.name === name) || null;
+    if (this.name === name) return this;
+    for (const child of this.offspring) {
+      const found = child.vampireWithName(name);
+      if (found) return found;
+    }
+    return null;
   }
 
   // Returns the total number of vampires that exist
